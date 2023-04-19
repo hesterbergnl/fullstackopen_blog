36c2e02ad007c709ba11e26c13ea81b54825547e
Nikolai Hesterberg <nikolai.hesterberg@gmail.com>
2023-04-19 21:39:09 +0200

    completed exercise 4.2 - refactored code to node standard structure

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const cors = require('cors')
+const blogRouter = require('./controllers/blogs')
+const moddleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+logger.info('connecting to', config.MONGODB_URI)
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/posts', blogRouter)
+
+module.exports = app
